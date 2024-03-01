81aa10b2ac0f517e745765d0ef8bc75ebd262f6e
mehtab ahmed <mehtab.ahmed@avl.com>
2024-03-01 22:39:24 +0100

    fix build issue

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,3 @@
+export default function Custormer(){
+   return <div>Customer</div>
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +7,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      {/* Here the the whole UI is mounted to the HTML Body Tag
+      equivalent to root mount in cra app
+      */}
+    <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
